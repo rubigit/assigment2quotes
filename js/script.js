a1b36412f56302809d80d91d36233fc540abbd9f
rubigit <rcortes1708.rcj@gmail.com>
2021-06-13 01:22:57 -0400

    Add feature to make the page responsive. Add style to all the html elements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log(`Ready`)
 
+    //list of language supported
+    const languages = [
+        { value: 'random', lan: 'Any language' },
+        { value: 'en', lan: 'English' },
+        { value: 'es', lan: 'Spanish' },
+        { value: 'pt', lan: 'Portuguese' },
+        { value: 'it', lan: 'Italian' },
+        { value: 'de', lan: 'German' },
+        { value: 'fr', lan: 'French' },
+        { value: 'cs', lan: 'Czech' },
+        { value: 'sk', lan: 'Slovak' },
+        { value: 'pl', lan: 'Polish' },
+        { value: 'hu', lan: 'Hungarian' },
+        { value: 'ru', lan: 'Russian' }
+    ]
+
+    // list the language supported to generate a quote
+    function fillLngQuote() {
+        let langTable = document.querySelector(`#lang-quote`)
+        languages.map(({ value, lan }) => {
+            let language = document.createElement(`option`)
+            language.value = value
+            language.innerHTML = lan
+            langTable.appendChild(language)
+        })
+    }
+
+    // list the language supported to filter and display historical quotes
+    function fillLngHistorical() {
+        let langTable = document.querySelector(`#lang-historial`)
+        languages.map(({ value, lan }) => {
+            let language = document.createElement(`option`)
+            language.value = value
+            language.innerHTML = lan
+            langTable.appendChild(language)
+        })
+    }
+
+    // "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
+    // "https://quotes15.p.rapidapi.com/quotes/random/",
+
+
     function quoteAPI() {
-        fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+        let language = `en`
+        let lanparameter = `?language_code=${language}`
+        let urlparameter = `random`
+        fetch(`https://quotes15.p.rapidapi.com/quotes/${urlparameter}/${lanparameter}`, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "122be4bc75msh68bce673fe682f2p115239jsnae37af3f51c1",
@@ -21,6 +66,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     quoteAPI()
+    fillLngQuote()
+    fillLngHistorical()
     console.log(`done`)
 
 })
